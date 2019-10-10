ee8e1eff57f7f9be2c97599671a299455eb98c93
Amad Hendro <ahendroo1@gmail.com>
2019-10-10 15:45:28 +0700

    first commit

---
+++ app/application/controllers/User.php
@@ -0,0 +1,159 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class User extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+
+
+
+    public function __construct(){
+
+		parent::__construct();
+		
+		$this->load->model('Login');
+		$this->load->library('session');
+    
+        ini_set('display_error','off');
+        error_reporting(0);
+    
+	}
+	
+	public function signin()
+	{
+		if($this->session->userdata('account_id')){
+			redirect(base_url());
+		}
+
+		if($this->input->post('email') && $this->input->post('password')){
+			
+			$email = $this->input->post('email');
+			$pass = $this->input->post('password');
+			$cek_login = $this->Login->CekLogin($email, $pass, 0);
+
+			if ($cek_login->num_rows()>0) {
+				# code...
+				foreach ($cek_login->result() as $row) {
+					# code...
+					$data = array(         'account_id' => $row->account_id,
+										   'name' => $row->name,
+										   'email' => $row->email,
+										   'password' => $row->password,
+										   'status' => $row->status,
+										   'role' => $row->role,
+										   'image' => $row->img_user,
+										   'ip_address' => $row->ip_address,
+										);
+   
+					$this->session->set_userdata($data);
+   
+					redirect(base_url());
+					// return 1 ;
+				}
+   
+			} else {
+
+				$this->session->set_flashdata( 'login_error', 'Email / Password salah');
+				redirect(base_url().'user/signin');
+
+			}
+
+
+		} else {
+
+			$this->load->view('signin');
+		}
+	}
+
+	public function signup()
+	{
+
+		if (
+
+			$this->input->post('full_name') && 
+			$this->input->post('phone_number') && 
+			$this->input->post('email') && 
+			$this->input->post('password') && 
+			$this->input->post('repeat_password')
+
+		) {
+
+			$full_name = $this->input->post('full_name') ;
+			$phone_number = $this->input->post('phone_number') ;
+			$email = $this->input->post('email') ;
+			$pass = $this->input->post('password');
+			# code...
+
+			if ($pass === $this->input->post('repeat_password') ){
+				
+				$cek_login = $this->Login->CekLogin($email, $pass, 1);
+
+				if ($cek_login->num_rows()>0) {
+
+					$this->session->set_flashdata('signup_alert', 'Email anda sudah terdaftar, silahkan <a href="'.base_url().'user/signin">Signin</a> ') ;
+					redirect(base_url().'user/signup');
+				} else {
+
+					$useraccount = array (
+						'name' => $full_name,
+						'phone_number' => $phone_number,
+						'email' => $email,
+						'password' => md5($this->input->post('password'))
+					);
+
+					$this->Login->signup($useraccount);
+					if(!$this->db->affected_rows()){
+
+						$this->session->set_flashdata('signup_alert', 'Ulangi Password tidak cocok') ;
+						redirect(base_url().'user/signup');
+
+					} else {
+
+						$this->session->set_flashdata('login_error', 'Registrasi Sukses, Silahkan login') ;
+						redirect(base_url().'user/signin');
+						
+					}
+				}
+				
+
+			} else {
+
+				$this->session->set_flashdata('repeat_pass', 'Ulangi Password tidak cocok') ;
+				$this->session->set_flashdata('full_name', $full_name ) ;
+				$this->session->set_flashdata('phone_number', $phone_number) ;
+				$this->session->set_flashdata('email', $email );
+				$this->session->set_flashdata('password', $this->input->post('password') );
+				$this->session->set_flashdata('repeat_password', $this->input->post('repeat_password')); 
+				$this->load->view('signup');
+
+			}
+
+		} else {
+
+			$this->load->view('signup');
+		}
+	}
+
+	public function logout(){
+
+
+		$this->session->sess_destroy();
+		redirect(base_url().'user/signin');
+	}
+
+
+}
---
+++ app/application/views/signin.php
@@ -0,0 +1,173 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Etrain</title>
+    <link rel="icon" href="img/favicon.png">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="<?php echo base_url() ?>asset/css/bootstrap.min.css">
+    <!-- animate CSS -->
+    <link rel="stylesheet" href="<?php echo base_url() ?>asset/css/animate.css">
+    <!-- owl carousel CSS -->
+    <link rel="stylesheet" href="<?php echo base_url() ?>asset/css/owl.carousel.min.css">
+    <!-- themify CSS -->
+    <link rel="stylesheet" href="<?php echo base_url() ?>asset/css/themify-icons.css">
+    <!-- flaticon CSS -->
+    <link rel="stylesheet" href="<?php echo base_url() ?>asset/css/flaticon.css">
+    <!-- font awesome CSS -->
+    <link rel="stylesheet" href="<?php echo base_url() ?>asset/css/magnific-popup.css">
+    <!-- swiper CSS -->
+    <link rel="stylesheet" href="<?php echo base_url() ?>asset/css/slick.css">
+    <!-- style CSS -->
+    <link rel="stylesheet" href="<?php echo base_url() ?>asset/css/style.css">
+</head>
+
+<body>
+  <!-- ================ contact section start ================= -->
+  <section class="contact-section section_padding_sign">
+    <div class="container">
+      <div class="row justify-content-center">
+          <div class="col-lg-6 sign_in">
+            <div class="section_tittle text-center">
+                <p></p>
+                <a href="<?php echo base_url() ?>">
+                  <img src="<?php echo base_url() ?>asset/img/logo.png" alt="Madiun NextGen" style="margin-left: auto; margin-right: auto;">
+                </a>
+                <h2></h2>
+            </div>
+            <p class="text-danger">
+              <?php echo $this->session->flashdata('login_error') ?>
+            </p>
+            <form class="form-contact" action="<?php echo base_url() ?>user/signin" method="post" >
+              <div class="row">
+                <div class="col-sm-12">
+                  <div class="form-group">
+                    <h6>Email</h6>
+                    <input class="form-control" name="email" id="email" type="email" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter your email'" placeholder = 'Enter your email' required="true">
+                  </div>
+                </div>
+                <div class="col-12">
+                  <div class="form-group">
+                    <h6>Password</h6>
+                    <input class="form-control" name="password" id="password" type="password" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter your password'" placeholder = 'Enter your password'>
+                  </div>
+                </div>
+              </div>
+              <div class="form-group">
+                <button type="submit" class="button btn_1" style="float: right;">Login</button>
+              </div>
+              <div style="padding-top: 5%; margin-bottom: 10px; float: right; position: relative; color: #b30ec2">
+                Belum punya akun? <a href="<?php echo base_url() ?>user/signup" style="color: #860791"> Daftar Sekarang! </a>
+              </div>
+            </form>
+          </div>
+      </div>
+    </div>
+  </section>
+  <!-- ================ contact section end ================= -->
+
+<!-- footer part start-->
+<footer class="footer-area">
+    <div class="container">
+        <div class="row justify-content-between">
+            <div class="col-sm-6 col-md-4 col-xl-3">
+                <div class="single-footer-widget footer_1">
+                    <a href="<?php echo base_url() ?>"> <img src="<?php echo base_url() ?>asset/img/logo.png" alt=""> </a>
+                    <p>But when shot real her. Chamber her one visite removal six
+                        sending himself boys scot exquisite existend an </p>
+                    <p>But when shot real her hamber her </p>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4 col-xl-4">
+                <div class="single-footer-widget footer_2">
+                    <h4>Newsletter</h4>
+                    <p>Stay updated with our latest trends Seed heaven so said place winged over given forth fruit.
+                    </p>
+                    <form action="#">
+                        <div class="form-group">
+                            <div class="input-group mb-3">
+                                <input type="text" class="form-control" placeholder='Enter email address'
+                                    onfocus="this.placeholder = ''"
+                                    onblur="this.placeholder = 'Enter email address'">
+                                <div class="input-group-append">
+                                    <button class="btn btn_1" type="button"><i class="ti-angle-right"></i></button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                    <div class="social_icon">
+                        <a href="#"> <i class="ti-facebook"></i> </a>
+                        <a href="#"> <i class="ti-twitter-alt"></i> </a>
+                        <a href="#"> <i class="ti-instagram"></i> </a>
+                        <a href="#"> <i class="ti-skype"></i> </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-sm-6 col-md-4">
+                <div class="single-footer-widget footer_2">
+                    <h4>Contact us</h4>
+                    <div class="contact_info">
+                        <p><span> Address :</span> Hath of it fly signs bear be one blessed after </p>
+                        <p><span> Phone :</span> +2 36 265 (8060)</p>
+                        <p><span> Email : </span>info@colorlib.com </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="copyright_part_text text-center">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <p class="footer-text m-0"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="ti-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- footer part end-->
+
+<!-- jquery plugins here-->
+<!-- jquery -->
+<script src="<?php echo base_url() ?>asset/js/jquery-1.12.1.min.js"></script>
+<!-- popper js -->
+<script src="<?php echo base_url() ?>asset/js/popper.min.js"></script>
+<!-- bootstrap js -->
+<script src="<?php echo base_url() ?>asset/js/bootstrap.min.js"></script>
+<!-- easing js -->
+<script src="<?php echo base_url() ?>asset/js/jquery.magnific-popup.js"></script>
+<!-- swiper js -->
+<script src="<?php echo base_url() ?>asset/js/swiper.min.js"></script>
+<script src="<?php echo base_url() ?>asset/js/jquery.nice-select.min.js"></script>
+<!-- swiper js -->
+<script src="<?php echo base_url() ?>asset/js/masonry.pkgd.js"></script>
+<!-- particles js -->
+<script src="<?php echo base_url() ?>asset/js/owl.carousel.min.js"></script>
+<!-- contact js -->
+<script src="<?php echo base_url() ?>asset/js/jquery.ajaxchimp.min.js"></script>
+<script src="<?php echo base_url() ?>asset/js/jquery.form.js"></script>
+<script src="<?php echo base_url() ?>asset/js/jquery.validate.min.js"></script>
+<script src="<?php echo base_url() ?>asset/js/mail-script.js"></script>
+<script src="<?php echo base_url() ?>asset/js/sign.js"></script>
+
+<!-- slick js -->
+<script src="<?php echo base_url() ?>asset/js/slick.min.js"></script>
+<script src="<?php echo base_url() ?>asset/js/jquery.counterup.min.js"></script>
+<script src="<?php echo base_url() ?>asset/js/waypoints.min.js"></script>
+<!-- custom js -->
+<script src="<?php echo base_url() ?>asset/js/custom.js"></script>
+</body>
+
+</html>
