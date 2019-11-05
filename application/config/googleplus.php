730381f6f201f1fd7b721b095ecd970e47d947ba
Amad Hendro <ahendroo1@gmail.com>
2019-11-05 20:12:02 +0700

    fiks login to google

--- application/config/googleplus.php
+++ application/config/googleplus.php
@@ -3,10 +3,19 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 $config['googleplus']['application_name'] = 'NextGenAcademy';
-$config['googleplus']['client_id']        = '155152934881-q0c8vs44j0etefqbtu2c5577bo3r1r5u.apps.googleusercontent.com';
-$config['googleplus']['client_secret']    = 'N04F77MhhMSRs0WrpAQbXai3';
-$config['googleplus']['redirect_uri']     = 'http://localhost/nextgenacademy/user/google_oauth';
+$config['googleplus']['client_id']        = '155152934881-1s4vcjpri0mv984u88a6psik07qh8a5f.apps.googleusercontent.com';
+$config['googleplus']['client_secret']    = 'nuRiwcmHSW5WQYFPDjnxrO-0';
+$config['googleplus']['redirect_uri']     = 'http://staging.nextgenacademy.id/user/google_oauth';
 $config['googleplus']['api_key']          = '';
 $config['googleplus']['scopes']           = array();
 
+
+// Local akses 
+
+// $config['googleplus']['application_name'] = 'NextGenAcademy';
+// $config['googleplus']['client_id']        = '155152934881-q0c8vs44j0etefqbtu2c5577bo3r1r5u.apps.googleusercontent.com';
+// $config['googleplus']['client_secret']    = 'N04F77MhhMSRs0WrpAQbXai3';
+// $config['googleplus']['redirect_uri']     = 'http://localhost/nextgenacademy/user/google_oauth';
+// $config['googleplus']['api_key']          = '';
+// $config['googleplus']['scopes']           = array();
 
