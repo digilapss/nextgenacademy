64180fb49f476487b35736d00ddd0b0f898779b5
setiyawan <setiyawan@gmail.com>
2019-11-10 14:04:20 +0700

    add env field

--- application/config/googleplus.php
+++ application/config/googleplus.php
@@ -2,20 +2,9 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['googleplus']['application_name'] = 'NextGenAcademy';
-$config['googleplus']['client_id']        = '155152934881-1s4vcjpri0mv984u88a6psik07qh8a5f.apps.googleusercontent.com';
-$config['googleplus']['client_secret']    = 'nuRiwcmHSW5WQYFPDjnxrO-0';
-$config['googleplus']['redirect_uri']     = 'http://staging.nextgenacademy.id/user/google_oauth';
-$config['googleplus']['api_key']          = '';
+$config['googleplus']['application_name'] = getenv('GP_APPLICATION_NAME');
+$config['googleplus']['client_id']        = getenv('GP_CLIENT_ID');
+$config['googleplus']['client_secret']    = getenv('GP_CLIENT_SECRET');
+$config['googleplus']['redirect_uri']     = getenv('GP_REDIRECT_URI');
+$config['googleplus']['api_key']          = getenv('GP_API_KEY');
 $config['googleplus']['scopes']           = array();
-
-
-// Local akses 
-
-// $config['googleplus']['application_name'] = 'NextGenAcademy';
-// $config['googleplus']['client_id']        = '155152934881-q0c8vs44j0etefqbtu2c5577bo3r1r5u.apps.googleusercontent.com';
-// $config['googleplus']['client_secret']    = 'N04F77MhhMSRs0WrpAQbXai3';
-// $config['googleplus']['redirect_uri']     = 'http://localhost/nextgenacademy/user/google_oauth';
-// $config['googleplus']['api_key']          = '';
-// $config['googleplus']['scopes']           = array();
-
